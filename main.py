7ddfce9886ffe8aad6864d290e52dcf01eaf74fe
Louis Albert Apas <louisalbertapas25@gmail.com>
2020-09-02 15:21:42 +0800

    Add additional trackbar example

--- main.py
+++ main.py
@@ -267,6 +267,32 @@ def trackbar():
     cv2.destroyAllWindows()
 
 
+def gray_color():
+    cv2.namedWindow('image')
+    cv2.createTrackbar('CP', 'image', 10, 400, nothing)
+
+    switch = 'color/gray'
+    cv2.createTrackbar(switch, 'image', 0, 1, nothing)
+
+    while True:
+        img = cv2.imread('lena.jpg')
+        pos = cv2.getTrackbarPos('CP', 'image')
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, str(pos), (50, 150), font, 4, (0, 0, 255), 10)
+        s = cv2.getTrackbarPos(switch, 'image')
+        if s == 0:
+            pass
+        else:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow('image', img)
+        k = cv2.waitKey(1) & 0xFF
+        if k == 27:  # esc key
+            break
+
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
     print("OpenCV")
-    trackbar()
+    gray_color()
